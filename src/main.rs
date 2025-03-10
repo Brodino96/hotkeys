c15e482bc825b856c651c41439500b98b0f84f0d
Brodino <brodino96@gmail.com>
2025-03-11 00:56:20 +0100

    Now with a random chance to say "Fuck you AutoHotKeys"

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![windows_subsystem = "windows"]
 
 use std::process::Command;
+use notify_rust::Notification;
+use rand::Rng;
 use win_hotkeys::HotkeyManager;
 use win_hotkeys::VKey;
 
@@ -9,11 +11,18 @@ fn main() {
    
     hkm.register_hotkey(VKey::Space, &[VKey::LWin], || {
         log::info!("Hotkey Win + Spacebar was pressed");
-        if let Err(e) = Command::new("nu")
-            .current_dir("C:\\Users\\brodi")
-            .spawn()
 
-        {
+        let mut rng = rand::rng();
+
+        if rng.random_bool(1.0/100.0) {
+            let _ = Notification::new()
+                .summary("AutoHotKeys sucks")
+                .body("Fuck you AutoHotKeys")
+                .show();
+        };
+
+        let task = Command::new("nu").current_dir("C:\\Users\\brodi").spawn();
+        if let Err(e) = task {
             log::error!("Failed to start Nushell: {}", e)
         }
     })
